c52af4ae4d15c5a2c423ac642a3ef77fa87ee417
Rahman <nijarr2020@gmail.com>
2019-11-16 23:46:09 +0530

    Startgamescreen completed

--- components/Header.js
+++ components/Header.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
     },
     headerTitle: {
         color: 'yellow',
-        fontWeight : 'bold',
+        fontWeight: 'bold',
         fontSize: 25
     }
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,20 +1,43 @@
 import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet, TouchableWithoutFeedback, KeyBoard } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Card from '../components/Card';
 import Input from '../components/Input';
 
 const StartGameScreen = () => {
     const [enteredValue, setEnteredValue] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelectedNumber] = useState();
 
     const inputHandler = inputText => {
         setEnteredValue(inputText.replace(/[^0-9]/g, ''));
     };
 
+    const resetHandler = () => {
+        setEnteredValue('');
+        setConfirmed(false);
+    };
+
+    const confirmHandler = () => {
+        const chosenNumber = parseInt(enteredValue);
+        if (isNaN(chosenNumber) ||  chosenNumber > 99 || chosenNumber === 0  ) {
+            return;
+        };
+        setConfirmed(true);
+        setSelectedNumber(parseInt(enteredValue));
+        setEnteredValue('');
+    };
+
+    let confirmedOutput;
+
+    if (confirmed) {
+        confirmedOutput = <Text> Chosen Number : {selectedNumber} </Text>
+    }
 
     return (
-        <TouchableWithoutFeedback onPress={() => { 
-            KeyBoard.dismiss();
-         }} >
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss();
+        }}
+        >
             <View style={styles.screen}>
                 <Text style={styles.title} > Start Game screen!! </Text>
                 <Text> Type a Number! </Text>
@@ -26,14 +49,16 @@ const StartGameScreen = () => {
                         maxLength={2}
                         keyboardType='number-pad'
                         style={styles.input}
+                        placeholder = 'Enter between 0 and 100..'
                         onChangeText={inputHandler}
                         value={enteredValue}
                     />
                     <View style={styles.buttonContainer} >
-                        <Button color='#c74657' title='Reset' onPress={() => { }} />
-                        <Button color='#4ecf71' title='Confirm' onPress={() => { }} />
+                        <Button color='#c74657' title='Reset' onPress={resetHandler} />
+                        <Button color='#4ecf71' title='Confirm' onPress={confirmHandler} />
                     </View>
                 </Card>
+                {confirmedOutput}
             </View>
         </TouchableWithoutFeedback>
     );
@@ -62,7 +87,7 @@ const styles = StyleSheet.create({
 
     },
     input: {
-        width: 100,
+        width: 200,
         textAlign: 'center'
 
     }
